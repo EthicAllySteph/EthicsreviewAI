d50259be1641384eb3914b8c1c13b0f1e4ea236e
EthicAllySteph <sgroh009@gold.ac.uk>
2025-06-24 22:56:36 +0200

    Update ethics-review.js

--- netlify/functions/ethics-review.js
+++ netlify/functions/ethics-review.js
@@ -1,5 +1,3 @@
-const https = require('https');
-
 exports.handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
     return {
@@ -10,34 +8,20 @@ exports.handler = async (event, context) => {
   }
 
   try {
-    // Test if we can make any external request at all
-    const testResponse = await new Promise((resolve, reject) => {
-      const req = https.request({
-        hostname: 'httpbin.org',
-        path: '/get',
-        method: 'GET'
-      }, (res) => {
-        let data = '';
-        res.on('data', chunk => data += chunk);
-        res.on('end', () => resolve({ status: res.statusCode, data }));
-      });
-      
-      req.on('error', reject);
-      req.setTimeout(5000, () => reject(new Error('Timeout')));
-      req.end();
-    });
-
+    // Test just importing fetch
+    const { default: fetch } = await import('node-fetch');
+    
     return {
       statusCode: 200,
       headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-      body: JSON.stringify({ message: 'External request works!', test: testResponse })
+      body: JSON.stringify({ message: 'node-fetch imported successfully' })
     };
 
   } catch (error) {
     return {
       statusCode: 500,
       headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-      body: JSON.stringify({ error: error.message })
+      body: JSON.stringify({ error: `Import error: ${error.message}` })
     };
   }
 };
