affad4c38da5a2f1c158180b77b02dac3668290a
EthicAllySteph <sgroh009@gold.ac.uk>
2025-06-25 08:47:18 +0200

    Update ethics-review.js

--- netlify/functions/ethics-review.js
+++ netlify/functions/ethics-review.js
@@ -1,16 +1,6 @@
 exports.handler = async (event, context) => {
-  console.log('Function called with method:', event.httpMethod);
-  
   return {
     statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ 
-      message: 'Hello from Netlify function',
-      method: event.httpMethod,
-      timestamp: new Date().toISOString()
-    })
+    body: "Hello World"
   };
 };
