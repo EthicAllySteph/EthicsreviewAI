7c763e3e9050ddecb982f6278c37a723383c20b8
EthicAllySteph <sgroh009@gold.ac.uk>
2025-06-25 08:53:24 +0200

    Update ethics-review.js

--- netlify/functions/ethics-review.js
+++ netlify/functions/ethics-review.js
@@ -1,7 +1,20 @@
 exports.handler = async (event, context) => {
+  console.log('Function called with method:', event.httpMethod);
+  
+  // Handle CORS preflight
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+      },
+      body: ''
+    };
+  }
+  
   try {
-    console.log('Function starting...');
-    
     const result = {
       statusCode: 200,
       headers: {
@@ -9,7 +22,8 @@ exports.handler = async (event, context) => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ 
-        message: 'Function working!',
+        message: 'Function working with CORS!',
+        method: event.httpMethod,
         timestamp: Date.now()
       })
     };
