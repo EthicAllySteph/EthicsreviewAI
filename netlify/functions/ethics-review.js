c6034c19dfc0880c69f98f841ca77861b00bbe7f
EthicAllySteph <sgroh009@gold.ac.uk>
2025-06-25 09:21:22 +0200

    Update ethics-review.js

--- netlify/functions/ethics-review.js
+++ netlify/functions/ethics-review.js
@@ -14,32 +14,14 @@ exports.handler = async (event, context) => {
     };
   }
   
-  try {
-    const result = {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ 
-        message: 'Function working with CORS!',
-        method: event.httpMethod,
-        timestamp: Date.now()
-      })
-    };
-    
-    console.log('Returning result:', result);
-    return result;
-    
-  } catch (error) {
-    console.log('Function error:', error);
-    return {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ error: error.message })
-    };
-  }
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ 
+      review: 'Test response from Netlify function - everything is working!'
+    })
+  };
 };
